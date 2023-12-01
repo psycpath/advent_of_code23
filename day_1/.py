d0b5fabf751d164b0eb7118addfebbd37e2bbc3e
Psychpath <ali.marmi@hotmail.com>
2023-12-01 10:05:44 +0100

    Day1Part 1 solved

--- day_1/.py
+++ day_1/.py
@@ -4,10 +4,14 @@ print('\nOutput for challenge day 1\n')
 with open('day_1/input.txt') as file:
     lines = file.readlines()
 
-
+sum_of_all_numbers = 0
 numbers = str()
 for line in lines:
     for char in line:
         if char.isdigit():
-            print(char)
+            numbers += char
+    sum_of_all_numbers += int(str(numbers[0]) + str(numbers[len(numbers)-1]))
+    numbers = str()
+
+print((sum_of_all_numbers))
             
