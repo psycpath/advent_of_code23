2284c51a774da7cd8ba05072bfd401b5271bcc68
Psychpath <ali.marmi@hotmail.com>
2023-12-01 07:39:57 +0100

    digit finder day1 complete

--- day_1/.py
+++ day_1/.py
@@ -4,4 +4,10 @@ print('\nOutput for challenge day 1\n')
 with open('day_1/input.txt') as file:
     lines = file.readlines()
 
-print(lines[0][0])
+
+numbers = str()
+for line in lines:
+    for char in line:
+        if char.isdigit():
+            print(char)
+            
