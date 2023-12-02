0189ced01f9b0946805f53a6d8040de9b0d33ffb
Psychpath <ali.marmi@hotmail.com>
2023-12-02 21:24:21 +0100

    Day2 part 1 solved

---
+++ day_2/.py
@@ -0,0 +1,37 @@
+print('\nOutput for challenge day 2\n')
+
+
+with open('day_2/input.txt') as file:
+    lines = file.readlines()
+
+# Determine which games would have been possible if the bag had been
+# loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. 
+# What is the sum of the IDs of those games?
+    
+    sum_of_ID = 0
+    
+for line in lines:
+    z = 0
+    round
+    
+    line = line.strip("\n")
+    ID = int(line.split(": ")[0].split()[1])
+    line = line.split(": ")[1]
+    games = line.split("; ")
+    
+    #cube = 
+    
+    for game in games:
+        round = game.split(", ")
+        for cube in round:
+            if cube[-1] == "d" and int(cube.split()[0]) > 12:
+                z += 1
+            elif cube[-1] == "n" and int(cube.split()[0]) > 13:
+                z += 1
+            elif cube[-1] == "e" and int(cube.split()[0]) > 14:
+                z += 1
+    if z == 0: 
+        sum_of_ID += ID
+    print(sum_of_ID)
+
+#3225: too High
--- tests.py
+++ tests.py
@@ -1,24 +1,35 @@
-dict = {
-    "one" : 1,
-    "two" : 2,
-    "three" : 3,
-    "four" : 4,
-    "five" : 5,
-    "six" : 6,
-    "seven" : 7,
-    "eight" : 8,
-    "nine" : 9,
-    "1" : 1,
-    "2" : 2,
-    "3" : 3,
-    "4":4,
-    "5":5,
-    "6":6,
-    "7":7,
-    "8":8,
-    "9":9
-    }
-for key in dict:
-    print(key)
+def is_possible(game_info, cube_counts):
+    for subset in game_info:
+        subset_counts = {color: 0 for color in cube_counts.keys()}
+        for item in subset:
+            color, count = item.split()
+            subset_counts[color] += int(count)
+        
+        if any(subset_counts[color] > cube_counts[color] for color in cube_counts.keys()):
+            return False
     
-print(len)
+    return True
+
+def possible_games(input_data, cube_counts):
+    possible_ids = []
+    for line in input_data:
+        game_id, game_info = line.split(": ")
+        game_info = [subset.split(", ") for subset in game_info.split("; ")]
+        
+        if is_possible(game_info, cube_counts):
+            possible_ids.append(int(game_id.split()[1]))
+
+    return possible_ids
+
+# Replace the input_data variable with your actual puzzle input
+input_data = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
+cube_counts = {'red': 12, 'green': 13, 'blue': 14}
+result = sum(possible_games(input_data, cube_counts))
+print(result)
